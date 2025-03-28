d128bfc4101715464bc258a16f9116212fe7e36c
neha <nehadevu36@gmail.com>
2025-03-28 12:29:59 +0530

    third commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import AdminLogin from './Components/Admin/AdminLogin';
+import AdminForgetpassword from './Components/Admin/AdminForgetpassword';
+import AdminResetpassword from './Components/Admin/AdminResetpassword';
+import AdminRegistration from './Components/Admin/AdminRegistration';
+import AdminDashboard from './Components/Admin/AdminDashboard';
 
 function App() {
   return (
     <div className="App">
      <AdminLogin/>
+     <AdminForgetpassword/>
+     <AdminResetpassword/>
+     <AdminRegistration/>
+     <AdminDashboard/>
     </div>
   );
 }
---
+++ client/src/Components/Admin/AdminForgetpassword.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import '../../Assets/Styles/AdminForgetpassword.css'
+
+function AdminForgetpassword() {
+  return (
+    <div>
+      
+
+
+    <div className="container">
+      <div className="card">
+        <h2 className="title">Forgot Password</h2>
+        <form>
+          <div className="input-group">
+            <label className="label">Email Address</label>
+            <input
+              type="email"
+              required
+              className="input"
+            />
+          </div>
+          <button type="submit" className="button">
+            Reset Password
+          </button>
+        </form>
+      </div>
+    </div>
+  
+
+    </div>
+  )
+}
+
+export default AdminForgetpassword
