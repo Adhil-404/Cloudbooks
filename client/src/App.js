17fc4792b009dd25e8416460453b1da9009be8fb
Adhil-404 <adhilh404@gmail.com>
2025-03-28 12:34:59 +0530

    fourth

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import AdminLogin from './Components/Admin/AdminLogin';
+import UserLogin from './Components/User/UserLogin';
+import UserRegistration from './Components/User/UserRegistration';
 
 function App() {
   return (
     <div className="App">
-     <AdminLogin/>
+     {/* <AdminLogin/> */}
+     <UserLogin/>
+     <UserRegistration/>
     </div>
   );
 }
